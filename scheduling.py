c640b6fb0ba9f004e9e9bd7b944991fbaaf4a375
João Victor <victor_campelo@outlook.com>
2019-10-14 10:03:14 -0300

    first commit

---
+++ scheduling.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def main():
+    # le o arquivo de entrada
+    data = sys.stdin.readlines()
+    print(data)
+    
+if __name__ == '__main__':
+    main()
